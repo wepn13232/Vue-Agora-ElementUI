9a3039a1cdc0f3f429b602fb1b5c57da5baf4b9a
linqiyuan <466214222@qq.com>
2019-10-17 18:28:02 +0800

    新增 文章信息页

--- src/views/essayInfo/essayInfo.vue
+++ src/views/essayInfo/essayInfo.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>这里是文章具体的详情页面</div>
+    <div class="essayInfo">
+        <!--文章卡片-->
+        <div class="essayCard">
+            <!--文章banner-->
+            <div class="cardBanner">
+                <!--banner内内容-->
+                <div class="bannerContent">
+                    <!--发布时间-->
+                    <div class="essay-time">
+                        <span>2019.10.17</span>
+                    </div>
+                    <!--标题-->
+                    <div class="essay-title">
+                        <h3>这里是标题</h3>
+                    </div>
+                    <!--作者-->
+                    <div class="author">
+                        <span>林启元</span>
+                    </div>
+                </div>
+            </div>
+            <!--文章内容-->
+            <div class="cardContent"></div>
+        </div>
+    </div>
 </template>
 
 <script src="./essayInfo.js"></script>
